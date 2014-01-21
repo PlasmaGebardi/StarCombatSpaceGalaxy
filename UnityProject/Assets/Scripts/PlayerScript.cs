9fd61e710bad705aca39273457ea489f46558461
PlasmaGebardi <matti.maatt@gmail.com>
2014-01-21 20:46:34 +0200

    Ship movement
    Added script for ship movement

---
+++ UnityProject/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    // speed value for the ship
+    public Vector2 speed = new Vector2(50, 50);
+
+    // for storing the movement
+    private Vector2 movement;
+	
+	// Update is called once per frame
+	void Update () {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(speed.x * inputX, speed.y * inputY);
+	}
+
+    void FixedUpdate()
+    {
+        rigidbody2D.velocity = movement;
+    }
+
+}
