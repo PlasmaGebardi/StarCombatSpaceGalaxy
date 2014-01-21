92180dc83a114933c08228efca6d8fa433936382
PlasmaGebardi <matti.maatt@gmail.com>
2014-01-21 23:15:04 +0200

    Rudimentary firing possible

---
+++ UnityProject/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+    // Total HP
+    public int hp = 2;
+
+    // Friend or foe, default for enemy
+    public bool isEnemy = true;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // is it a shot?
+        ShotScript shot = collider.gameObject.GetComponent<ShotScript>();
+        if (shot != null)
+        {
+            // no friendly fire
+            if (shot.isEnemyShot != isEnemy)
+            {
+                hp -= shot.damage;
+
+                // upon collision, the shot is destroyed
+                Destroy(shot.gameObject);
+
+                if (hp <= 0) { Destroy(gameObject); }
+            }
+        }
+    }
+
+}
---
+++ UnityProject/Assets/Scripts/ShotScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotScript : MonoBehaviour {
+
+    // Damage of shot
+    public int damage = 1;
+
+    // Projectile damage enemy?
+    public bool isEnemyShot = false;
+
+	// Use this for initialization
+	void Start () {
+        // ttl for the bullet in seconds
+        Destroy(gameObject, 5);
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+}
