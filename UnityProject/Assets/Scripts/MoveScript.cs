8263dbdc5cc8ad7ebd3ee5b9b1442ce1e2aa23df
PlasmaGebardi <matti.maatt@gmail.com>
2014-01-21 21:14:43 +0200

    Fixed line endings and added enemies

---
+++ UnityProject/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+    // speed of the object
+    public Vector2 speed = new Vector2(10, 10);
+
+    // static movement direciton
+    public Vector2 direction = new Vector2(-1, 0);
+
+    // vector for storing movement info
+    public Vector2 movement;
+	
+	// Update is called once per frame
+	void Update () {
+        movement = new Vector2(
+            speed.x * direction.x,
+            speed.y * direction.y);
+	}
+
+    void FixedUpdate()
+    {
+        rigidbody2D.velocity = movement;
+    }
+}
