8e90c215e43a51276a5fcbd94bacd2a179b40852
mmekbe <mmekbe@utu.fi>
2014-01-22 16:48:39 +0200

    Ensimmäinen versio GitHubiin
    Toiminnot: Scrollaus, Ampuminen vahingolla (player & enemy),
    Törmääminen, !CollisionDmg, Vihollisten herääminen tullessa kameraan

---
+++ New Unity Project/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// Handle hitpoints and damages
+public class HealthScript : MonoBehaviour
+{
+	/// Total hitpoints
+	public int hp = 2;
+	
+	/// Enemy or player?
+	public bool isEnemy = true;
+	
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		// Is this a shot?
+		ShotScript shot = collider.gameObject.GetComponent<ShotScript>();
+		if (shot != null)
+		{
+			// Avoid friendly fire
+			if (shot.isEnemyShot != isEnemy)
+			{
+				hp -= shot.damage;
+				
+				// Destroy the shot
+				// Remember to always target the game object,
+				// otherwise you will just remove the script.
+				Destroy(shot.gameObject);
+				
+				if (hp <= 0)
+				{
+					// Dead!
+					Destroy(gameObject);
+				}
+			}
+		}
+	}
+}
