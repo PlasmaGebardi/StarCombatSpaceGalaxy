77326f579a2b0e06778b94f91489d983db387952
PlasmaGebardi <matti.maatt@gmail.com>
2014-01-22 16:20:24 +0200

    Added collision damage
    Enemies will be always destroyed in collision with player

--- UnityProject/Assets/Scripts/HealthScript.cs
+++ UnityProject/Assets/Scripts/HealthScript.cs
@@ -9,10 +9,14 @@ public class HealthScript : MonoBehaviour {
     // Friend or foe, default for enemy
     public bool isEnemy = true;
 
+    // damage inflicted on collision
+    public int collDam = 1;
+
     void OnTriggerEnter2D(Collider2D collider)
     {
         // is it a shot?
         ShotScript shot = collider.gameObject.GetComponent<ShotScript>();
+        HealthScript health = collider.gameObject.GetComponent<HealthScript>();
         if (shot != null)
         {
             // no friendly fire
@@ -23,6 +27,18 @@ public class HealthScript : MonoBehaviour {
                 // upon collision, the shot is destroyed
                 Destroy(shot.gameObject);
 
+                if (hp <= 0) { Destroy(gameObject); }
+            }
+        }
+        else if (health != null)
+        {
+            // no friendly fire
+            if (health.isEnemy != isEnemy)
+            {
+                hp -= health.collDam;
+
+                // upon collision, check if collible object
+                if (health.isEnemy) { Destroy(health.gameObject); }
                 if (hp <= 0) { Destroy(gameObject); }
             }
         }
