4e5d22d4d246c279cbe41ffd5fe5212f7d373bde
PlasmaGebardi <matti.maatt@gmail.com>
2014-01-26 20:53:46 +0200

    Modded HealthScript

--- StarCombat/Assets/Scripts/HealthScript.cs
+++ StarCombat/Assets/Scripts/HealthScript.cs
@@ -1,37 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/// Handle hitpoints and damages
-public class HealthScript : MonoBehaviour
-{
-	/// Total hitpoints
-	public int hp = 2;
-	
-	/// Enemy or player?
-	public bool isEnemy = true;
-	
-	void OnTriggerEnter2D(Collider2D collider)
-	{
-		// Is this a shot?
-		ShotScript shot = collider.gameObject.GetComponent<ShotScript>();
-		if (shot != null)
-		{
-			// Avoid friendly fire
-			if (shot.isEnemyShot != isEnemy)
-			{
-				hp -= shot.damage;
-				
-				// Destroy the shot
-				// Remember to always target the game object,
-				// otherwise you will just remove the script.
-				Destroy(shot.gameObject);
-				
-				if (hp <= 0)
-				{
-					// Dead!
-					Destroy(gameObject);
-				}
-			}
-		}
-	}
+public class HealthScript : MonoBehaviour {
+
+    // Total HP
+    public int hp = 2;
+
+    // Friend or foe, default for enemy
+    public bool isEnemy = true;
+
+    // is it an asteroid
+    public bool isAsteroid = false;
+
+    // damage inflicted on collision
+    public int collDam = 1;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // is it a shot?
+        ShotScript shot = collider.gameObject.GetComponent<ShotScript>();
+        HealthScript health = collider.gameObject.GetComponent<HealthScript>();
+        if (shot != null)
+        {
+            // no friendly fire
+            if (shot.isEnemyShot != isEnemy)
+            {
+                hp -= shot.damage;
+
+                // upon collision, the shot is destroyed
+                Destroy(shot.gameObject);
+
+                if (hp <= 0) { Destroy(gameObject); }
+            }
+        }
+        else if (health != null)
+        {
+            // no friendly fire
+            if (health.isEnemy != isEnemy)
+            {
+                hp -= health.collDam;
+
+                // upon collision, check if collible object
+                if (health.isEnemy) { Destroy(health.gameObject); }
+
+                // destroy player if health reaches zero
+                if (hp <= 0) { Destroy(gameObject); }
+            }
+        }
+    }
+
 }
