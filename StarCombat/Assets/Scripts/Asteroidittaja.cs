fb7a269ead6e0d8effbc2e14e0c7f39bd0fe1369
PlasmaGebardi <matti.maatt@gmail.com>
2014-01-29 21:16:14 +0200

    Added asteroidittaja, asteroid rotater

---
+++ StarCombat/Assets/Scripts/Asteroidittaja.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroidittaja : MonoBehaviour {
+
+	// Use this for initialization
+	//void Start () { NO EI VITTU MITÄÄN }
+
+    public float rpm = 10.0f;
+
+    
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 0, 6 * rpm * Time.deltaTime);
+	}
+}
