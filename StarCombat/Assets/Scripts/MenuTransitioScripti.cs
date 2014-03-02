6e8b948a3cd9ea8e0c8094a1cfa8438c87619a50
PlasmaGebardi <matti.maatt@gmail.com>
2014-03-03 00:26:20 +0200

    death now results in transition to menu in 3seconds

---
+++ StarCombat/Assets/Scripts/MenuTransitioScripti.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class MenuTransitioScripti : MonoBehaviour {
+
+    private float counter = 0f;
+    //public bool ded = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    //public void Menuun() { ded = true; }
+	// Update is called once per frame
+    void Update()
+    {
+
+        if (!GameObject.Find("Player"))
+        {
+            counter += Time.deltaTime;
+            if ((int)counter > 3) { Application.LoadLevel("Menu"); }
+        }
+    }
+}
