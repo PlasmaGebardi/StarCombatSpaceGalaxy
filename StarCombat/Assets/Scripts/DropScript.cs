6bc3522e222aec400165631b241be431325466ff
PlasmaGebardi <matti.maatt@gmail.com>
2014-03-02 22:09:09 +0200

    DropScript added
    dropit nyt toimii ja hävii oikein

---
+++ StarCombat/Assets/Scripts/DropScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropScript : MonoBehaviour {
+
+    public int HpBoost = 1;
+
+/*	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}*/
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        PlayerScript playa = collider.gameObject.GetComponent<PlayerScript>();
+
+        if (playa != null)
+        {
+            HealthScript helat = playa.gameObject.GetComponent<HealthScript>();
+            helat.hp += HpBoost;
+            Destroy(gameObject);
+        }
+    }
+}
